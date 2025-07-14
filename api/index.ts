7ac89e1426d603af6e28ca5eebf4414dc608a97f
Thanapon Wongprasert <thanaponwps@gmail.com>
2025-07-15 01:12:01 +0700

    update vercel deploy

---
+++ api/index.ts
@@ -0,0 +1,28 @@
+import { Hono } from "hono";
+import { logger } from "hono/logger";
+import { handle } from "hono/vercel";
+import api from "../src/api";
+
+const app = new Hono();
+
+app.use("*", logger());
+
+app.get("/", (c) => {
+  return c.text("🎉 Welcome to the Hono API! 🎉");
+});
+
+app.route("/api", api);
+
+app.notFound((c) => {
+  return c.json({ message: "Not Found", ok: false }, 404);
+});
+
+app.onError((err, c) => {
+  console.error(`${err}`);
+  return c.json({ message: "An internal error occurred", ok: false }, 500);
+});
+
+export const GET = handle(app);
+export const POST = handle(app);
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,28 @@
-import { Hono } from "hono";
-import { logger } from "hono/logger";
-import { handle } from "hono/vercel";
-import api from "./api";
+// import { Hono } from "hono";
+// import { logger } from "hono/logger";
+// import { handle } from "hono/vercel";
+// import api from "./api";
 
-const app = new Hono();
+// const app = new Hono();
 
-app.use("*", logger());
+// app.use("*", logger());
 
-app.get("/", (c) => {
-  return c.text("🎉 Welcome to the Hono API! 🎉");
-});
+// app.get("/", (c) => {
+//   return c.text("🎉 Welcome to the Hono API! 🎉");
+// });
 
-app.route("/api", api);
+// app.route("/api", api);
 
-app.notFound((c) => {
-  return c.json({ message: "Not Found", ok: false }, 404);
-});
+// app.notFound((c) => {
+//   return c.json({ message: "Not Found", ok: false }, 404);
+// });
 
-app.onError((err, c) => {
-  console.error(`${err}`);
-  return c.json({ message: "An internal error occurred", ok: false }, 500);
-});
+// app.onError((err, c) => {
+//   console.error(`${err}`);
+//   return c.json({ message: "An internal error occurred", ok: false }, 500);
+// });
 
-export const GET = handle(app);
-export const POST = handle(app);
+// export const GET = handle(app);
+// export const POST = handle(app);
 
-export default app;
+// export default app;
