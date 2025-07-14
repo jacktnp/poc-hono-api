5bb0b4f28682fe147ba165728edacefb135d9ac5
Thanapon Wongprasert <thanaponwps@gmail.com>
2025-07-15 01:20:38 +0700

    test poc

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,48 @@
 import { Hono } from "hono";
-import { logger } from "hono/logger";
 import { handle } from "hono/vercel";
-import api from "./api";
 
 export const config = {
   runtime: "edge",
 };
 
-// const app = new Hono();
 const app = new Hono().basePath("/api");
 
-app.use("*", logger());
-
 app.get("/", (c) => {
-  return c.text("🎉 Welcome to the Hono API! 🎉");
+  return c.json({ message: "Hello Hono!" });
 });
 
-app.route("/api", api);
+export default handle(app);
 
-app.notFound((c) => {
-  return c.json({ message: "Not Found", ok: false }, 404);
-});
+// import { Hono } from "hono";
+// import { logger } from "hono/logger";
+// import { handle } from "hono/vercel";
+// import api from "./api";
 
-app.onError((err, c) => {
-  console.error(`${err}`);
-  return c.json({ message: "An internal error occurred", ok: false }, 500);
-});
+// export const config = {
+//   runtime: "edge",
+// };
 
-export const GET = handle(app);
-export const POST = handle(app);
+// // const app = new Hono();
+// const app = new Hono().basePath("/api");
 
-// export default app;
+// app.use("*", logger());
+
+// app.get("/", (c) => {
+//   return c.text("🎉 Welcome to the Hono API! 🎉");
+// });
+
+// app.route("/api", api);
+
+// app.notFound((c) => {
+//   return c.json({ message: "Not Found", ok: false }, 404);
+// });
+
+// app.onError((err, c) => {
+//   console.error(`${err}`);
+//   return c.json({ message: "An internal error occurred", ok: false }, 500);
+// });
+
+// export const GET = handle(app);
+// export const POST = handle(app);
+
+// // export default app;
