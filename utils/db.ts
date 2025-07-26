5cb3bc21b569199d779ede754ae1d9c2ae8fa60b
Thanapon Wongprasert <thanaponwps@gmail.com>
2025-07-26 22:59:11 +0700

    update db config

--- utils/db.ts
+++ utils/db.ts
@@ -2,5 +2,5 @@
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 
-const sql = neon(process.env.DATABASE_URL!);
+const sql = neon(process.env.NEON_DATABASE_URL!);
 export const db = drizzle({ client: sql });
