910ce8309e81375fc2e18a45ceecdbc29ad00053
Thanapon Wongprasert <thanaponwps@gmail.com>
2025-07-26 23:09:29 +0700

    update test

--- utils/db.ts
+++ utils/db.ts
@@ -1,6 +1,15 @@
-// src/db.ts
+// connect neon
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 
 const sql = neon(process.env.NEON_DATABASE_URL!);
 export const db = drizzle({ client: sql });
+
+// connect pg
+// import { drizzle } from "drizzle-orm/node-postgres";
+// import { Pool } from "pg";
+
+// const pool = new Pool({
+//   connectionString: process.env.NEON_DATABASE_URL ?? "",
+// });
+// export const db = drizzle(pool);
